4f614a2296a7248d1ca2880288da5613082563fb
Juho-Veikko Hytönen <jv.hytonen@gmail.com>
2023-01-16 13:43:55 +0200

    First commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,23 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  const [data, setData] = useState(null)
+  useEffect(() => {
+   const fetchData = async () => {
+           const response = await fetch('https://restcountries.com/v3.1/all')
+           const data = await response.json()
+           console.log(data)
+   }
+   fetchData()
+  })
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <p>The app</p>
       </header>
     </div>
   );
